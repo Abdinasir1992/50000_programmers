c9bf5ee79673ca6142ece8a157bc131443ce0888
Abdinasir <abdinasir150892@gmail.com>
2022-03-18 22:50:06 +0600

    Задание 1 и задание 2

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\HomeController;
 
 use App\Http\Controllers\PostController;
 
+use App\Http\Controllers\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,3 +28,11 @@ Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/edit', [PostController::class, 'edit']);
 
 Route::get('posts/delete', [PostController::class, 'delete']);
+
+Route::get('/products/', [ProductController::class, 'index']);
+
+Route::get('/products/create', [ProductController::class, 'create']);
+
+Route::get('/products/edit', [ProductController::class, 'edit']);
+
+Route::get('/products/delete', [ProductController::class, 'delete']);
